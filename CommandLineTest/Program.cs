6c12544eec005741a986123bc538492459f1a552
bbowen23 <benjamingeorgebowen@gmail.com>
2021-05-18 13:58:47 -0500

    new file:   .gitignore 	new file:   CommandLineTest/CommandLineTest.csproj 	new file:   CommandLineTest/CommandLineTest.sln 	new file:   CommandLineTest/Program.cs

---
+++ CommandLineTest/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CommandLineTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello from the command line!");
+            Console.WriteLine("If this repository is clean, then this test has succeeded.");
+        }
+    }
+}
